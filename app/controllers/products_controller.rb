506529a6a4a8df8dc4c9332b1b112d985a4dc754
Dmg5th <Dmgould310@gmail.com>
2020-10-05 14:35:08 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,12 @@ class ProductsController < ApplicationController
     def index
     end 
 
-    def show 
-    end 
+   def add
+    cart << params[:product]
+    render :index
+   end 
+
+ 
+
+
 end
